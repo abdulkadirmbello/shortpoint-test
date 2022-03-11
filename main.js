f2dbf8854dd24f35999a6b797ee7f0c3acc586bc
Abdulkadir Muhammad Bello <99006829+abdulkadirmbello@users.noreply.github.com>
2022-03-11 21:52:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-let i = 0;
-function fire(){
-    if( i == 0 ){
-        let width = 50;
-        let progress = document.querySelector(".progress");
-        let interval = setInterval(run, 10);
-        function run(){
-            if(width != 100){
-                width++;
-                progress.style.width = width + "%";
-                progress.innerHTML = width + "%";
+const finishBtn = document.querySelector('#finish')
+const innerDivs = document.querySelectorAll('.inner')
+
+// eventlistener for button click
+finishBtn.addEventListener('click', () => {
+    let output = 50;
+    let interval = setInterval(run, 10);
+
+    function run() {
+        for (i = 0; i <= innerDivs.length; i++) {
+            innerDivs[i].textContent = `${output}%`;
+            innerDivs[i].style.width = `${output}%`;
+            if (output != 100) {
+                output++;
             }else{
                 clearInterval(interval);
-                i=0;
             }
         }
     }
-    
-}
+});
